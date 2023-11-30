05ad2d489f5b113ef72993a8965b8def721686ee
Daniel <daniel.dean.mist@gmail.com>
2023-11-30 14:22:50 -0500

    key

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import time
 app = Flask(__name__)
 
 # Replace with the actual Optimism RPC URL
-optimism_rpc_url = 'https://linea-mainnet.infura.io/v3/e2b4d9fa19c748489fb6c0d6bf411be4'
+optimism_rpc_url = 'infura_key'
 
 # Create a Web3 instance to connect to the Optimism blockchain
 web3 = Web3(Web3.HTTPProvider(optimism_rpc_url))
